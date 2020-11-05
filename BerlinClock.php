2c6ea38f2cb2a03fcf54bf7467d5f15d0f946422
Giuseppe <g_amatulli@outlook.com>
2020-11-05 18:37:02 +0100

    refactor - count first line hours hours%5 equals 1 should return A E E E

--- BerlinClock.php
+++ BerlinClock.php
@@ -58,7 +58,7 @@ class BerlinClock
     }
 
     function firstLineHours (int $hours) : string {
-        if($hours == 1 || $hours==6)
+        if($hours%5 == 1)
             return "A E E E";
         if($hours==2)
             return "A A E E";
