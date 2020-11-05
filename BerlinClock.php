1593b7092e8dab529df9d07ca2c622587207a994
Giuseppe <g_amatulli@outlook.com>
2020-11-05 12:50:26 +0100

    add - count first line input 0 minute

--- BerlinClock.php
+++ BerlinClock.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace BerlinClock;
 
 class BerlinClock
 {
-
+    function firstLineMinutes (int $minute) : string {
+        return "E E E E";
+    }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,7 +1,32 @@
 <?php
 
+require "vendor/autoload.php";
+require "BerlinClock.php";
+
+use PHPUnit\Framework\TestCase;
+use BerlinClock\BerlinClock;
+
+
 
 class BerlinClockTest extends PHPUnit_Framework_TestCase
 {
+
+    private $berlinClock;
+
+
+
+    protected function setUp(): void{
+        $this->berlinClock = new BerlinClock();
+    }
+
+    public function test_first_line_minutes_given0_shouldReturnEEEE(){
+        //Arrange
+
+        //Act
+        $actual = $this->berlinClock->firstLineMinutes(0);
+
+        //Assert
+        $this->assertEquals("E E E E",$actual);
+    }
 
 }
