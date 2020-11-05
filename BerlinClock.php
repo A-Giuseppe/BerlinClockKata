52e875c06398327479e94d1a5eb8b6e40a904284
Giuseppe <g_amatulli@outlook.com>
2020-11-05 14:49:00 +0100

    refactor - count first line minutes%5 equals 2 should return A A E E

--- BerlinClock.php
+++ BerlinClock.php
@@ -7,14 +7,12 @@ class BerlinClock
     function firstLineMinutes (int $minute) : string {
         if($minute%5 == 1)
             return "A E E E";
-        elseif ($minute ==2 )
+        elseif ($minute%5 == 2)
             return  "A A E E";
         elseif ( $minute == 3)
             return "A A A E";
         elseif ($minute == 4 )
             return "A A A A";
-        elseif ($minute == 7)
-            return "A A E E";
         return "E E E E";
     }
 }
